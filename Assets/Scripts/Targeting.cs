5877b42c097a4c54c6f5878fe0479c272b618e8e
Darren Farr <dmf6737@rit.edu>
2017-04-27 20:48:45 -0400

    Area ready for playtest.

---
+++ Assets/Scripts/Targeting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Reports back that a clickable gameobject has been clicked on to select it as a target.
+/// </summary>
+public class Targeting : MonoBehaviour
+{
+    //Private Variables
+    private GameManager gm;
+
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start ()
+    {
+        gm = GameManager.Instance;
+	}
+
+
+    /// <summary>
+    /// Tells game manager that I have been targeted.
+    /// </summary>
+    private void OnMouseDown()
+    {
+        //Tell game manager that I am the current target.
+        //gm.SetTarget(gameObject);
+    }
+
+}//end of Targeting
