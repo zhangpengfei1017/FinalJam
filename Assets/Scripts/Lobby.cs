ab9f6743c7ea4f1578c23137e02c1159675cdc96
Sravan Kumar <knight2356@gmail.com>
2017-05-05 23:29:45 -0400

    New Lobby scene using the assets.

---
+++ Assets/Scripts/Lobby.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using UnityEngine.UI;
+
+// This script is to take care of Lobby backend and is to be called by the UI.
+
+public class Lobby : PunBehaviour {
+
+    public static Lobby _instance;
+    public GameObject RoomPanel;
+
+    private string username;
+    private string _selectedRoomName;
+
+    private void Awake()
+    {
+        if (null == Lobby._instance)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        PhotonNetwork.ConnectUsingSettings("1.0f");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setUserName(UILabel _uiText)
+    {
+        username = _uiText.text;
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        // Get and Refresh Rooms List here.
+        int i = 0;
+        Debug.Log("dsa");
+        Debug.Log(PhotonNetwork.GetRoomList().Length);
+        foreach (var item in PhotonNetwork.GetRoomList())
+        {
+            Vector3 tempPos = new Vector3(RoomPanel.transform.position.x, RoomPanel.transform.position.y + i * 50, RoomPanel.transform.position.z);
+            GameObject newObj = Instantiate(RoomPanel, tempPos, Quaternion.identity);
+            Debug.Log(newObj);
+            ++i;
+        }
+
+    }
+
+    public void SelectRoom(UILabel _text)
+    {
+        _selectedRoomName = _text.text;
+    }
+
+    public void CreateRoomButton(UILabel _text)
+    {
+        RoomOptions RO = new RoomOptions();
+        RO.MaxPlayers = 5;
+        PhotonNetwork.CreateRoom(_text.text, RO, TypedLobby.Default);
+    }
+}
---
+++ Assets/Scripts/LobbyCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyCamera : MonoBehaviour {
+
+    public Transform LoginObject;
+    public Transform LobbyObj;
+    public Transform CreateRoomPopup;
+    public Transform RoomObj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveToLobby()
+    {
+        transform.position = LobbyObj.position;
+    }
+
+    public void MoveToCreateRoomPopup()
+    {
+        transform.position = CreateRoomPopup.position;
+    }
+
+    public void MoveToRoom()
+    {
+        transform.position = RoomObj.transform.position;
+    }
+}
