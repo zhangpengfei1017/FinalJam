2db858cddf66be9e3a988d4ebe48729da1e889e8
zhangpengfei1017 <zhangpengfei1017@gmail.com>
2017-05-07 23:19:05 -0400

    Add some UI in gameplay scene

---
+++ Assets/GameplayUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayUI : MonoBehaviour {
+    public UIGrid teammates;
+    public GameObject teammateFrame;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
